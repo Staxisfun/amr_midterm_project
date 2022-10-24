b98f77f174f1a7748a5c07497d6ababd7092a48f
aldwinb <aldwinb23@gmail.com>
2022-10-23 19:49:23 -0700

    create listings route in server.js

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
 const favoritesRoutes = require('./routes/favorites');
+const listingsRoutes = require('./routes/listings');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -40,6 +41,7 @@ app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 app.use('/favorites', favoritesRoutes);
+app.use('/listings', listingsRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
