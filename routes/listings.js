f32a7f03f5fbba5569e0985a16860070cf2402d1
aldwinb <aldwinb23@gmail.com>
2022-10-26 00:34:38 -0700

    style listing page, implement function to grab id in listings.js

--- routes/listings.js
+++ routes/listings.js
@@ -2,6 +2,23 @@ const express = require('express');
 const { getListings, getListing } = require('../db/queries/listings');
 const router  = express.Router();
 
+
+router.get('/create', (req, res) => {
+  res.render('listings-create');
+});
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  console.log("listings id:", id)
+  getListing(id).then((data) => {
+    console.log("listing data:", data);
+    return res.render('listing', {data});
+  })
+  res.render('listing');
+
+})
+
+
 router.get('/', (req, res) => {
   // let listings = JSON.stringify(getListings())
   let listings = [];
@@ -13,22 +30,22 @@ router.get('/', (req, res) => {
     // res.render('listings', {listings:listings});
   })
   console.log("listing: ", listings)
-
 });
 
-router.get('/create', (req, res) => {
-  res.render('listings-create');
-});
-
-router.get('/:id', (req, res) => {
-  getListing().then((data) => {
-    console.log("listing data:", data);
-    res.render('listing', {data});
-  })
-  res.render('listing');
-})
 
 
 
+// router.get('/:id', (req, res) => {
+//   getListing().then((data) => {
+//     console.log("listing data:", data);
+//     res.render('listing', {data});
+//   })
+//   res.render('listing');
+// })
 
-module.exports = router;
+
+
+
+
+
+    module.exports = router;
