31426beac972f45340c6cb75d70027f9cb2ce566
aldwinb <aldwinb23@gmail.com>
2022-10-26 22:43:05 -0700

    fix routes to show all username

--- routes/listings.js
+++ routes/listings.js
@@ -13,38 +13,39 @@ router.get('/create', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const listingID = req.params.id
-  const userID = +req.headers.cookie.split("=")[1]; // take cookies from header -- '+' coverts it to number
+  const userID = +req.headers.cookie.split("=")[1];
   getUserById(userID).then((user) =>{
     getListing(listingID).then((data) => {
-      res.render('listing', {data : {...data, ...user, ...{currentUserId: userID}}});
+      res.render('listing', { data, user });
     })
   })
 })
 
 
+router.get('/', (req, res) => {
+  const userID = +req.headers.cookie.split("=")[1]; // take cookies from header -- '+' coverts it to number
+  getUserById(userID).then((user) =>{
+    getAllListings()
+    .then((data) => {
+      res.render('listings', {data, user});
+    })
+  })
+});
 
-// router.get('/', (req, res) => {
-//   const userID = +req.headers.cookie.split("=")[1];    // take cookies from header -- '+' coverts it to number
-//   getUserById(userID).then((user) =>{
-//     getListings().then((data) => {
-//       res.render('listings', {data, user});
-//     })
 
 router.post('/', (req, res) => {
   let min = req.body.lowest_price * 100;
   let max = req.body.highest_price * 100;
-  getListings(min, max)
+  const userID = +req.headers.cookie.split("=")[1]; // take cookies from header -- '+' coverts it to number
+  getUserById(userID).then((user) =>{
+    getListings(min, max)
     .then((data) => {
-      res.render('listings', {data});
+      res.render('listings', {data, user});
     })
-});
-
-router.get('/', (req, res) => {
-  getAllListings()
-  .then((data) => {
-    res.render('listings', {data});
   })
 });
+
+
 
 
 module.exports = router;
