1d2cff818544e43c061cae67f2f2337a78437b51
Ray Yu <kaito-akira@hotmail.com>
2022-10-26 21:38:09 -0700

    change database structure

--- db/queries/listings.js
+++ db/queries/listings.js
@@ -1,11 +1,40 @@
 const db = require('../connection');
 
-const getListings = () => {
+const getListings = (min, max) => {
+  if(!max) {
+    return db.query(`
+    SELECT * FROM listings
+    WHERE price >= $1;`, [min])
+      .then(listings => {
+        return listings.rows;
+      });
+  }
+
+  if (!min) {
+    return db.query(`
+  SELECT * FROM listings
+  WHERE price <= $1;`, [max])
+    .then(listings => {
+      return listings.rows;
+    });
+  }
+
+  return db.query(`
+    SELECT * FROM listings
+    WHERE price >= $1
+    AND price <= $2;`, [min, max])
+    .then(listings => {
+      return listings.rows;
+  });
+};
+
+const getAllListings = () => {
   return db.query('SELECT * FROM listings;')
     .then(listings => {
       return listings.rows;
     });
 };
+
 
 const getListing = (id) => {
   return db.query('SELECT * FROM listings WHERE id = $1;', [id])
@@ -14,4 +43,4 @@ const getListing = (id) => {
     });
 };
 
-module.exports = { getListings, getListing };
+module.exports = { getListings, getListing, getAllListings };
