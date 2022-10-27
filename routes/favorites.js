17fc7a663d3875ab501e61f58a813d51432283b1
aldwinb <aldwinb23@gmail.com>
2022-10-26 21:33:38 -0700

    change user to display username

--- routes/favorites.js
+++ routes/favorites.js
@@ -3,34 +3,24 @@ const express = require("express");
 const router = express.Router();
 const { getFavorites } = require("../db/queries/favorites");
 const { getListings, getListing } = require("../db/queries/listings");
+const { getUserById } = require('../db/queries/users');
 
-// router.get("/", (req, res) => {
-//   getListings().then((data) => {
-//     console.log("data: ", data);
-//     res.render("favorites", { data });
-//   });
-// });
+
 
 router.get("/", (req, res) => {
   const listingID = req.params.id
   const userID = +req.headers.cookie.split("=")[1];
   console.log('userID=== ', userID);
-  getFavorites(userID)
-    .then((data) => {
-      console.log("favorites data===", data);
-      res.render("favorites", { data });
-    });
+  getUserById(userID).then((user) =>{
+    getFavorites(userID)
+      .then((data) => {
+        console.log("favorites data===", data);
+        console.log("user:", user);
+        res.render("favorites", { data, user });
+      });
+    })
 });
 
-//filter by price range
-// router.get("/filter", (req, res) => {
-//   // const min = req.query.min
-//   // const max = req.query.max
 
-//   getFilteredByPrice(null, null, '10')
-//     .then(data => {
-//      res.render("favorites", { data });
-//   })
-// })
 
 module.exports = router;
--- routes/listings.js
+++ routes/listings.js
@@ -1,51 +1,39 @@
 const express = require('express');
 const { getListings, getListing } = require('../db/queries/listings');
+const { getUserById } = require('../db/queries/users');
 const router  = express.Router();
 
-
 router.get('/create', (req, res) => {
-  res.render('listings-create');
+  const userID = +req.headers.cookie.split("=")[1];    // take cookies from header -- '+' coverts it to number
+  getUserById(userID).then((user) =>{
+    res.render('listings-create', {user});
+  })
 });
-
 
 
 
 router.get('/:id', (req, res) => {
   const listingID = req.params.id
-  const userID = +req.headers.cookie.split("=")[1];
-  // take cookies from header -- '+' coverts it to number
-  // console.log("userID:", userID);
-  // console.log("listings id:", listingID)
-
-  getListing(listingID).then((data) => {
-    res.render('listing', {data : {...data, ...{currentUserId: userID}}});
+  const userID = +req.headers.cookie.split("=")[1]; // take cookies from header -- '+' coverts it to number
+  getUserById(userID).then((user) =>{
+    getListing(listingID).then((data) => {
+      res.render('listing', {data : {...data, ...user, ...{currentUserId: userID}}});
+    })
   })
-
 })
 
 
 
-
 router.get('/', (req, res) => {
-  getListings().then((data) => {
-    res.render('listings', {data});
+  const userID = +req.headers.cookie.split("=")[1];    // take cookies from header -- '+' coverts it to number
+  getUserById(userID).then((user) =>{
+    getListings().then((data) => {
+      res.render('listings', {data, user});
+    })
   })
 });
 
 
 
+module.exports = router;
 
-// router.get('/:id', (req, res) => {
-//   getListing().then((data) => {
-//     console.log("listing data:", data);
-//     res.render('listing', {data});
-//   })
-//   res.render('listing');
-// })
-
-
-
-
-
-
-    module.exports = router;
