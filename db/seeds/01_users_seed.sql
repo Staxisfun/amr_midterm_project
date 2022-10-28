6e25ae6903a831a18e1d9ed9de394e68c85407a0
Matt Wilson <mwilson82@hotmail.com>
2022-10-28 07:40:03 +0000

    last database update before demo day

--- db/seeds/01_users_seed.sql
+++ db/seeds/01_users_seed.sql
@@ -1,12 +1,4 @@
 
 -- Users table seeds here (Example)
-INSERT INTO users (name, email, password, is_admin) VALUES ('Ray Yu', 'gryu325@gmail.com', 'hulk', false);
-INSERT INTO users (name, email, password, is_admin) VALUES ('Tony Stark', 'gryu325@gmail.com', 'ironman', false);
-INSERT INTO users (name, email, password, is_admin) VALUES ('Thor', 'gryu325@gmail.com', 'thor', false);
-INSERT INTO users (name, email, password, is_admin) VALUES ('Steve Rogers', 'gryu325@gmail.com', 'captain', false);
-INSERT INTO users (name, email, password, is_admin) VALUES ('Peter Parker', 'gryu325@gmail.com', 'spiderman', false);
-INSERT INTO users (name, email, password, is_admin) VALUES ('Natalia Romanova', 'gryu325@gmail.com', 'blackwidow', false);
-INSERT INTO users (name, email, password, is_admin) VALUES ('Wanda Maximoff', 'gryu325@gmail.com', 'scarlet', false);
-INSERT INTO users (name, email, password, is_admin) VALUES ('Ray Yu', 'gryu325@gmail.com', 'rayyu', true);
-INSERT INTO users (name, email, password, is_admin) VALUES ('Matt Wilson', 'gryu325@gmail.com', 'matt', true);
+INSERT INTO users (name, email, password, is_admin) VALUES ('Ray Yu', 'gryu325@gmail.com', 'ray', false);
 INSERT INTO users (name, email, password, is_admin) VALUES ('Aldwin Bautista', 'gryu325@gmail.com', 'aldwin', true);
--- db/seeds/03_favorites.sql
+++ db/seeds/03_favorites.sql
@@ -1,11 +1,9 @@
 
-INSERT INTO favorites (user_id, listing_id) VALUES (4, 1);
-INSERT INTO favorites (user_id, listing_id) VALUES (4, 2);
-INSERT INTO favorites (user_id, listing_id) VALUES (4, 5);
-INSERT INTO favorites (user_id, listing_id) VALUES (4, 10);
+INSERT INTO favorites (user_id, listing_id) VALUES (2, 1);
+INSERT INTO favorites (user_id, listing_id) VALUES (2, 2);
+INSERT INTO favorites (user_id, listing_id) VALUES (2, 5);
+INSERT INTO favorites (user_id, listing_id) VALUES (2, 10);
 INSERT INTO favorites (user_id, listing_id) VALUES (1, 3);
 INSERT INTO favorites (user_id, listing_id) VALUES (1, 5);
 INSERT INTO favorites (user_id, listing_id) VALUES (1, 8);
 INSERT INTO favorites (user_id, listing_id) VALUES (1, 9);
-INSERT INTO favorites (user_id, listing_id) VALUES (10, 10);
-INSERT INTO favorites (user_id, listing_id) VALUES (10, 1);
